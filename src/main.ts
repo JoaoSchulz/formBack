36bd8bd39477dcb514aa658cc8c184474688ecb0
JoaoSchulz <joaoguilherme.da@gmail.com>
2025-05-21 14:00:10 -0300

    resolvendo erros 2

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    'http://localhost:3000', // Para desenvolvimento local
-    'https://formulario-front-five.vercel.app', // Origem anterior
-    'https://formulario-front-9qxdayrt7-joao-schulzs-projects.vercel.app', // Nova origem
-  ];
-
+  // Configuração de CORS para aceitar todas as origens
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.error('Blocked by CORS:', origin); // Log de erro
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Inclua o método OPTIONS
+    origin: '*', // Permitir todas as origens
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
     credentials: true, // Permitir cookies ou autenticação
     allowedHeaders: ['Content-Type', 'Authorization'], // Permitir cabeçalhos necessários
   });
